4801cb5852f15178ae3ba3a6e2ebb7e2c882d89b
Jannes Behrens <jannescb@me.com>
2021-06-16 23:02:04 +0200

    add docblock to interface

--- src/Contracts/UrlParser.php
+++ src/Contracts/UrlParser.php
@@ -4,5 +4,11 @@ namespace AwStudio\Indexer\Contracts;
 
 interface UrlParser
 {
+    /**
+     * Get the URLs html.
+     *
+     * @param string $url
+     * @return string
+     */
     public function getHtml(string $url);
 }
